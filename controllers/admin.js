5a94a7b6610373ef3526c838162a48c22db5bea4
tommer <tamber@redhat.com>
2020-10-10 16:11:02 +0300

    added 2 new routes, one for the 'getEditProduct', that populates the fields in the html if we press the 'edit' btn in the admin-products page. Furthermore, we changed the edit btn to include the var prodId and an optional query parameter for ?edit=true. in the controller we made the changes to handle the request for the new page. We also merged the two ejs files of 'add-product' and the 'edit-product' because they contain the same html elements , but the differeance is that the add-product elements are empty and in the edit-page they are filled because we chose an existing product. we are using ejs dynamic rendering to pupulate the page accordingly.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,19 @@
 const Product = require('../models/product');
 
 // !!!!!!!!!!!!! Middleware functions !!!!!!!!!!!!!
+
+/* 
+    both the add-product and edit-product paegs has the same html, but one is empty (add new product entirly)]
+    and the other has the input fields already filled because the product already exist
+    if you press "edit" on some product you will be redirected to another route function, not to here
+    router.post('/add-product
+*/
 exports.getAddProductPage = (req, res, next) => {
     // relative to app.js
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        editing: false
     });
 };
 
@@ -20,6 +25,42 @@ exports.postNewProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+//router.get('/edit-product/:productId' 
+exports.getEditProductPage = (req, res, next) => {
+    /* 
+        req.query ==> data passed to the server from client in the URL!!
+        .edit ==> http://mysite/admin/edit-product/<productId>?edit=<data that i want to check>
+        typeof(<data that i want to check>) == string (always), so use casting accordingly 
+        req.query information is optional, so we dont need to find it in the route, but we do need to check its existance here
+    */
+    const editMode = req.query.edit;
+    // editMode == empty (client side didnt pass that key-value pair in the URL)
+    if (!editMode) {
+        res.redirect('/');
+    }
+
+    // in route ==> /edit-product/:productId
+    const prodId = req.params.productId;
+    Product.findById(prodId, (product) => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        // relative to app.js
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            // nothing is going to be highlighted in the nav bar 
+            path: '/admin/edit-product',
+            // editmode == "true"/"false" (string)
+            editing: editMode,
+            product: product
+        });
+    });
+};
+
+exports.postEditProduct = (res, req, next) => {
+
 };
 
 exports.getProducts = (req, res, next) => {
--- routes/admin.js
+++ routes/admin.js
@@ -14,4 +14,9 @@ router.post('/add-product', adminController.postNewProduct);
 // /admin/products => GET (filtering to the /admin path already takes place in app.js)
 router.get('/products', adminController.getProducts);
 
+// /admin/edit-product/{productId} => GET (filtering to the /admin path already takes place in app.js)
+router.get('/edit-product/:productId', adminController.getEditProductPage);
+
+// /admin/edit-product => POST (filtering to the /admin path already takes place in app.js)
+router.post('/edit-product', adminController.postEditProduct);
 module.exports = router;
